d6a9d63be3aedfa17ba85abe846d26cfca2a461a
Diego Fernández Barrera <bigomby@gmail.com>
2016-06-25 07:11:30 +0200

    Disable fleetKey methods

--- common/models/fleet-key.js
+++ common/models/fleet-key.js
@@ -1,3 +1,23 @@
 module.exports = (FleetKey) => {
+  FleetKey.disableRemoteMethod('create', true);
+  FleetKey.disableRemoteMethod('upsert', true);
+  FleetKey.disableRemoteMethod('updateAll', true);
+  FleetKey.disableRemoteMethod('updateAttributes', false);
 
+  FleetKey.disableRemoteMethod('find', true);
+  FleetKey.disableRemoteMethod('findById', true);
+  FleetKey.disableRemoteMethod('findOne', true);
+
+  FleetKey.disableRemoteMethod('deleteById', true);
+
+  FleetKey.disableRemoteMethod('count', true);
+  FleetKey.disableRemoteMethod('exists', true);
+
+  FleetKey.disableRemoteMethod('confirm', true);
+  FleetKey.disableRemoteMethod('resetPassword', true);
+  FleetKey.disableRemoteMethod('logout', true);
+
+  FleetKey.disableRemoteMethod('createChangeStream', true);
+
+  FleetKey.disableRemoteMethod('__get__user', false);
 };
